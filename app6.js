845a71e3e172a07d7f9e7f6f8d976cbce4586a6a
German von Scheidt <germanvscheidt@gmail.com>
2022-05-19 18:48:32 -0300

    Primer comit, se agrego el archivo Html y JS

---
+++ app6.js
@@ -0,0 +1,91 @@
+'use strict'
+
+// let n1 = parseInt(prompt('Ingrese un numero'));
+// let n2 = parseInt(prompt('Ingrese otro numero'));
+// let n3 = parseInt(prompt('Ingrese otro numero mas'));
+
+
+// if (n1 > n2 && n1 > n3)
+// {
+//     document.write('El mayor de los numeros es' + n1);
+// }
+// else if (n2 > n3)
+// {
+//     document.write('El mayor de los numeros es ' + n2);
+// }
+// else
+// {
+//     document.write('El mayor es: ' + n3);
+// }
+
+// ejercicio 8
+
+// let n1 = parseFloat(prompt('ingrese un numero'));
+// if(n1 % 2 === 0)
+// {
+//     document.write('el resto es 0')
+// }
+
+// ejercicio 9 
+
+// let frase = prompt('ingresa una frase');
+// console.log(`las vocales que aparecen en la frase son:  + ${frase}`);
+
+// if (frase.includes('a'))
+// {
+//     console.log('a');
+// }
+
+
+
+// if (frase.includes('e'))
+// {
+//     console.log('e');
+// }
+
+
+// if (frase.includes('i'))
+// {
+//     console.log('i');
+// }
+
+
+// if (frase.includes('o'))
+// {
+//     console.log('o');
+// }
+
+
+// if (frase.includes('u'))
+// {
+//     console.log('u');
+// }
+
+// ejercicio 10
+
+// var n1 = parseInt(prompt('ingrese un numero'));
+
+// if(n1 % 2 === 0 || n1 % 3 === 0 || n1 % 5 === 0 || n1 % 7 === 0)
+// {
+//     if(n1 % 2 === 0)
+//     {
+//         document.write('Es divisible por 2. ');
+//     }
+//     if(n1 % 3 === 0)
+//     {
+//         document.write('Es divisible por 3. ');
+//     }
+//     if(n1 % 5 === 0)
+//     {
+//         document.write('Es divisible por 5. ');
+//     }
+//     if(n1 % 7 === 0)
+//     {
+//         document.write('Es divisible por 7. ');
+//     }
+    
+// }
+// else
+//     {
+//         document.write('El numero no es divisible por ninguna de las opciones');
+//     }
